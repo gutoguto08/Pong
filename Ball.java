73269c4a20d7ffdd244ce5473757717bbafb4890
gutoguto08 <53495830+gutoguto08@users.noreply.github.com>
2020-12-03 18:43:21 -0600

    Create Ball.java

---
+++ Ball.java
@@ -0,0 +1,15 @@
+package com.mycompany.testingground;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Ball extends JPanel {
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public Ball () {
+		
+	}
+}
